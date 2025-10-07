3c53f49d5a6c51f0c95782e24367cae374ba579f
bitturana-ab <b2ra830@gmail.com>
2025-10-07 22:43:23 +0530

    variable and their max or min value

--- src/Main.kt
+++ src/Main.kt
@@ -2,9 +2,20 @@ fun main(){
     val name = "Leo Das"
     var age = 20
     age = 48
+    val integer = 21474836488
     println("Hello $name your age is $age")
     println("Integer max value is ${Int.MAX_VALUE}")
     println("Integer min value is ${Int.MIN_VALUE}")
-    println("Integer min value is ${Int.MAX_VALUE+1}")
+//    this shouldn't run but worked
+    println("Integer value is $integer")
+    println("Byte max value is ${Byte.MAX_VALUE}")
+    println("Byte min value is ${Byte.MIN_VALUE}")
+    println("Short max value is ${Short.MAX_VALUE}")
+    println("Short min value is ${Short.MIN_VALUE}")
+    println("Long max value is ${Long.MAX_VALUE}")
+    println("Long min value is ${Long.MIN_VALUE}")
+    println("Float min value is ${Float.MIN_VALUE}")
+    println("Float max value is ${Float.MAX_VALUE}")
+
 
 }
