37c0a0366dc652053af5206ec25a2f17e8f150fd
bitturana-ab <b2ra830@gmail.com>
2025-10-07 23:22:28 +0530

    increment decrement have done and control flow will start

--- src/operatorsCondition.kt
+++ src/operatorsCondition.kt
@@ -8,15 +8,23 @@ fun main(){
     println("x * Y = ${x / y}")
     println("x % Y = ${x % y}")
 
-    x = x + 2
+    // x = x + 2
+    x += 2
     println("x = x + 2 = ${x}")
-    x = x - 2
+    x -= 2
     println("x = x - 2 = ${x}")
-    x = x * 2
+    x *= 2
     println("x = x * 2 = ${x}")
-    x = x / 2
+    x /= 2
     println("x = x / 2 = ${x}")
-    x = x % 2
+    x %= 2
     println("x = x % 2 = ${x}")
+
+//    increment operator post and pre
+    println("x++ = ${x++}")
+    println("++x = ${++x}")
+//    decrement
+    println("x-- = ${x--}")
+    println("--x = ${--x}")
 
 }
