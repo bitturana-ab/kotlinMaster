aec345430285bafe9edd1c8f5d1b479b4057c5ff
bitturana-ab <b2ra830@gmail.com>
2025-10-07 22:30:05 +0530

    integer max and min value

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,10 @@
 fun main(){
-    val name: String = "Leo Das"
-    var age: Int = 20
+    val name = "Leo Das"
+    var age = 20
     age = 48
     println("Hello $name your age is $age")
+    println("Integer max value is ${Int.MAX_VALUE}")
+    println("Integer min value is ${Int.MIN_VALUE}")
+    println("Integer min value is ${Int.MAX_VALUE+1}")
+
 }
