80a537cd2cb774089fd740b1d4a1a04e865b206e
bitturana-ab <b2ra830@gmail.com>
2025-10-07 22:55:33 +0530

    char,boolean also all types

--- src/variables.kt
+++ src/variables.kt
@@ -1,6 +1,12 @@
 fun main(){
     val doubleNumber: Double = 2.5
     val floatNumber: Float = 2.5F
+    val floatNumber2 = 28F
+    println(floatNumber2)
     println(doubleNumber)
     println(floatNumber)
+    val charValue: Char = 'A'
+    println(charValue)
+    val booleanValue = true;
+    println(booleanValue);
 }
