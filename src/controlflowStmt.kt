fbd1cd70594bc90d188a738410cd1b23948d2868
bitturana-ab <b2ra830@gmail.com>
2025-10-08 20:54:19 +0530

    if statement done

---
+++ src/controlflowStmt.kt
@@ -0,0 +1,11 @@
+fun main(){
+    if(true){
+        println("True statement")
+    }else{
+        println("False statement")
+    }
+
+    val ifValue = if(false)"False" else "True"
+    println(ifValue)
+
+}
