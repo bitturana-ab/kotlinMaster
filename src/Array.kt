3050f503376067fa061152522032bb03f6bf8051
bitturana-ab <b2ra830@gmail.com>
2025-10-08 21:26:33 +0530

    defined type of array

--- src/Array.kt
+++ src/Array.kt
@@ -1,11 +1,11 @@
 fun main(){
-    val names = arrayOf("Sanjay","vijay","AB")
+    val names: Array<String> = arrayOf("Sanjay","vijay","AB")
     println(names[1])
     for(name in names)print("$name ")
 //    mixed typed array
     println()
-    val numbers = arrayOf(28,25,10,"Ancy","Sanjay",'A','B')
-    for(num in numbers){
+    val mixedElements = arrayOf(28,25,10,"Ancy","Sanjay",'A','B')
+    for(num in mixedElements){
 //        print("$num ")
 //        check is char or not
         if (num is Char)print("$num ")
