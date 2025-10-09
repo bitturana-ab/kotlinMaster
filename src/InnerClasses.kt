c79532ef510419a423ba5b20c9c31c863176a6b1
bitturana-ab <b2ra830@gmail.com>
2025-10-09 13:37:32 +0530

    account details and transaction with inner class and fun

--- src/InnerClasses.kt
+++ src/InnerClasses.kt
@@ -1,6 +1,36 @@
+import kotlin.math.acos
+
 fun main(){
     var listView = ListView(arrayOf("sanjay","vijay","ab","leo das","jd"))
     listView.ListViewItem().displayItem(3)
+
+//    Account section
+    var account = Account("Leo Das",10000)
+    account.validate().withdraw(20000)
+}
+
+//challenge -> Account username, amount, transaction, checkAmount
+class Account(val username: String,val amount: Int){
+    var balance:Int = amount
+    inner class validate(){
+        fun withdraw(amount: Int){
+            if (balance < 0){
+                println("Insufficient balance")
+                return
+            }
+            if(balance < amount) {
+                println("Min balance")
+                return
+            }
+            balance -= amount
+            println("Transaction successfully of amount $amount")
+            println("Available balance is = $balance")
+
+
+        }
+
+    }
+
 }
 
 class ListView(val lists: Array<String>){
