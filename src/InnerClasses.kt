cd7d9c7d7daece33d31802eaec851960a88861e6
bitturana-ab <b2ra830@gmail.com>
2025-10-09 22:46:42 +0530

    updated trans

--- src/InnerClasses.kt
+++ src/InnerClasses.kt
@@ -6,31 +6,40 @@ fun main(){
 
 //    Account section
     var account = Account("Leo Das",10000)
-    account.validate().withdraw(20000)
+    account.withdraw(2000)
 }
 
 //challenge -> Account username, amount, transaction, checkAmount
 class Account(val username: String,val amount: Int){
+//    private but
     var balance:Int = amount
-    inner class validate(){
-        fun withdraw(amount: Int){
-            if (balance < 0){
-                println("Insufficient balance")
-                return
-            }
-            if(balance < amount) {
-                println("Min balance")
-                return
-            }
-            balance -= amount
-            println("Transaction successfully of amount $amount")
-            println("Available balance is = $balance")
+    var transactions = mutableListOf<Int>()
 
-
+    fun deposit(amount: Int){
+        if(amount > 0){
+            transactions.add(amount)
+            balance += amount
+            println("$amount deposited. Balance is now ${this.balance} ")
         }
-
+        else{
+            println("Deposit should be more than 0")
+        }
     }
 
+    fun withdraw(amount: Int){
+        if (balance < 0){
+                println("Insufficient balance")
+                return
+        }
+        if(balance < amount) {
+                println("Min balance")
+                return
+        }
+        balance -= amount
+        transactions.add(-balance)
+        println("Transaction successfully of amount $amount")
+        println("Available balance is = $balance")
+        }
 }
 
 class ListView(val lists: Array<String>){
