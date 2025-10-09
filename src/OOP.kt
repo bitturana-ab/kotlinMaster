80df31c363cd25dcaa75e71d0cb93e36c0015bad
bitturana-ab <b2ra830@gmail.com>
2025-10-09 09:18:06 +0530

    constructor and params constructor

--- src/OOP.kt
+++ src/OOP.kt
@@ -5,6 +5,10 @@ fun main(){
     println(car.name)
     car.move()
 
+//    paramsCar
+    var car2 = ParamsCar("Kontessa","mini","25k")
+    car2.move()
+
 }
 class Car{
     var name=""
@@ -16,5 +20,14 @@ class Car{
     }
     fun stop(){
         println("Car has stopped")
+    }
+}
+//parameter constructor
+class ParamsCar(name: String,model:String,price:String){
+    val name = name
+    val model = model
+    val price = price
+    fun move(){
+        println("$name car is moving")
     }
 }
