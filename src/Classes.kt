e7fc34d15be6527b15563761052e2b18ffa03ea2
bitturana-ab <b2ra830@gmail.com>
2025-10-09 13:08:09 +0530

    anum class  get data

--- src/Classes.kt
+++ src/Classes.kt
@@ -4,9 +4,24 @@ fun main(){
     User("ancy",28)
     User(age =  28)
 //    enum class
-    println(Direction.NORTH)
-    println(Direction.SOUTH)
+//    println(Direction.NORTH)
+    println(Direction.NORTH.direction)
+    println(Direction.NORTH.distance)
+    println(Direction.NORTH.name)
+    Direction.SOUTH.getData()
 }
+
+enum class Direction(var direction: String,var distance: Int){
+    NORTH("north",10),
+    SOUTH("south",20),
+    EAST("east",20),
+    WEST("west",30);
+    fun getData(){
+        println("Direction = $direction and distance = $distance")
+    }
+}
+
+
 //created object in OOP file
 //class User(name: String,var age: Int){
 ////    constructor for only name params and set default age 28
@@ -42,6 +57,6 @@ class User(name: String="unknown",var age: Int=20){
 
 }
 //enum class means fixed constant for selecting
-enum class Direction{
-    NORTH,SOUTH,EAST,WEST
-}
+//enum class Direction{
+//    NORTH,SOUTH,EAST,WEST
+//}
