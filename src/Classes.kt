56f0865ec064ea42b1364966c2282f45630852e9
bitturana-ab <b2ra830@gmail.com>
2025-10-09 10:24:15 +0530

    multiple constructor by kotlin in classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -2,14 +2,30 @@
 fun main(){
     User("Sanjay")
     User("ancy",28)
-    User(28)
+    User(age =  28)
 }
 //created object in OOP file
-class User(name: String,var age: Int){
-//    constructor for only name params and set default age 28
-    constructor(name: String):this(name,28)
-//    can have multiple but diff params
-    constructor(age: Int):this("user",age)
+//class User(name: String,var age: Int){
+////    constructor for only name params and set default age 28
+//    constructor(name: String):this(name,28)
+////    can have multiple but diff params
+//    constructor(age: Int):this("user",age)
+//    var name: String
+//    init {
+//        if(name.lowercase().startsWith("a")){
+//            this.name = name
+//            println("Name: ${this.name} and age: $age")
+//        }else{
+//            this.name = "user"
+//            println("User $name doesn't start with 'a' or 'A' ")
+//        }
+//    }
+//
+//}
+
+//auto constructor by kotlin
+
+class User(name: String="unknown",var age: Int=20){
     var name: String
     init {
         if(name.lowercase().startsWith("a")){
@@ -17,7 +33,7 @@ class User(name: String,var age: Int){
             println("Name: ${this.name} and age: $age")
         }else{
             this.name = "user"
-            println("User $name doesn't start with 'a' or 'A' ")
+            println("User $name doesn't start with 'a' or 'A' and default age: $age ")
         }
     }
 
