376d0cdcbf83866ab9c6f8ec73b89f33bc4ade8f
bitturana-ab <b2ra830@gmail.com>
2025-10-09 09:59:36 +0530

    constructor in classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,10 +1,17 @@
+//for checking create main fun here
+fun main(){
+    User("Sanjay")
+    User("ancy","29")
+}
 //created object in OOP file
 class User(name: String,var age: String){
+//    constructor for only name params and set default age 28
+    constructor(name: String):this(name,"28")
     var name: String
     init {
         if(name.lowercase().startsWith("a")){
             this.name = name
-            println("Name: ${this.name}")
+            println("Name: ${this.name} and age: $age")
         }else{
             this.name = "user"
             println("User $name doesn't start with 'a' or 'A' ")
